33eb9ae20aaeff541f6c045e846f0bb3828f7b15
Robmargar <roberto.mar.garc@gmail.com>
2024-11-07 23:48:12 -0600

    Se mejora el menú se le colocan sombras a los iconos de git Link y descarga

--- js/index.js
+++ js/index.js
@@ -12,69 +12,69 @@ const spinnerDB2 = document.querySelectorAll(".double-bounce2");
 let sppinerStatus = true;
 
 document.addEventListener("DOMContentLoaded", () => {
-    wellcome.classList.add("wellcome--opacity");
+  wellcome.classList.add("wellcome--opacity");
 });
 
 iconNav.addEventListener("click", () => {
-    nav.classList.toggle("nav__enlaces__visible");
+  nav.classList.toggle("nav__enlaces__visible");
 });
 
 opacityTransitionElement(imgPerfil);
 opacityElement(imgPerfil);
 
 xp.forEach((element) => {
-    opacityTransitionElement(element);
-    opacityElement(element);
+  opacityTransitionElement(element);
+  opacityElement(element);
 });
 
 spinner.forEach((element) => {
-    opacityTransitionElement(element);
+  opacityTransitionElement(element);
 });
 
 function opacityElement(element) {
-    const observer = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-            element.style.opacity = "1";
+  const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      element.style.opacity = "1";
 
-            if (element.classList.contains("article__experiencia")) {
-                hiddenShowSpinner();
-                sppinerStatus = false;
-                return;
-            }
-        } else {
-            element.style.opacity = "0";
-        }
-    });
+      if (element.classList.contains("article__experiencia")) {
+        hiddenShowSpinner();
+        sppinerStatus = false;
+        return;
+      }
+    } else {
+      element.style.opacity = "0";
+    }
+  });
 
-    observer.observe(element);
+  observer.observe(element);
 }
 
 // FUNCIONES
 function opacityTransitionElement(element) {
-    element.classList.add("opacityElement");
+  element.classList.add("opacityElement");
 }
 
 function hiddenShowSpinner() {
-    if (sppinerStatus) {
-        spinnerStyle(spinner, "opacity", "1");
-        spinnerStyle(spinner, "display", "block");
+  if (sppinerStatus) {
+    spinnerStyle(spinner, "opacity", "1");
+    spinnerStyle(spinner, "display", "block");
 
-        spinnerStyle(spinnerDB1, "animationPlayState", "running");
-        spinnerStyle(spinnerDB2, "animationPlayState", "running");
+    spinnerStyle(spinnerDB1, "animationPlayState", "running");
+    spinnerStyle(spinnerDB2, "animationPlayState", "running");
 
-        setTimeout(() => {
-            spinnerStyle(spinner, "opacity", "0");
+    setTimeout(() => {
+      spinnerStyle(spinner, "opacity", "0");
 
-            spinnerStyle(spinnerDB1, "animationPlayState", "paused");
-            spinnerStyle(spinnerDB2, "animationPlayState", "paused");
+      spinnerStyle(spinnerDB1, "animationPlayState", "paused");
+      spinnerStyle(spinnerDB2, "animationPlayState", "paused");
 
-            setTimeout(() => {
-                spinner.forEach((e) => e.remove());
-            }, 2000);
-        }, 2000);
-    }
+      setTimeout(() => {
+        spinner.forEach((e) => e.remove());
+      }, 2000);
+    }, 2000);
+  }
 }
 
 function spinnerStyle(element, styleCustom, valueCustom) {
-    element.forEach((e) => (e.style[styleCustom] = `${valueCustom}`));
+  element.forEach((e) => (e.style[styleCustom] = `${valueCustom}`));
 }
